fea969c9cc1a05d68a986e290d1bab7a1d75bf93
Ana Neves <asf25@sussex.ac.uk>
2025-04-17 19:20:32 +0100

    finished data_cleaning script

--- analysis/preprocessing.R
+++ analysis/preprocessing.R
@@ -5,7 +5,8 @@ path <- "C:/Users/asf25/Box/IllusionPCS/"
 
 # JsPsych experiment ------------------------------------------------------
 
-files <- list.files(path, pattern = "*.csv")
+# files <- list.files(path, pattern = "*.csv")
+files <- "z9650zj48m.csv"
 
 # Progress bar
 progbar <- progress_bar$new(total = length(files))
@@ -15,8 +16,6 @@ alldata_ig <- data.frame()
 
 for (file in files) {
   progbar$tick()
-  
-  # file <- "tnr9kq0qja.csv"
   rawdata <- read.csv(paste0(path, "/", file))
   
   # Initialize participant-level data
@@ -27,7 +26,7 @@ for (file in files) {
     Recruitment = dat$researcher,
     # Condition = dat$condition,
     Experiment_StartDate = as.POSIXct(paste(dat$date, dat$time), format = "%d/%m/%Y %H:%M:%S"),
-    # Experiment_Duration = rawdata[rawdata$screen == "demographics_debrief", "time_elapsed"] / 1000 / 60,
+    Experiment_Duration = rawdata[rawdata$screen == "waitdatasaving", "time_elapsed"] / 1000 / 60,
     Browser_Version = paste(dat$browser, dat$browser_version),
     Mobile = dat$mobile,
     Platform = dat$os,
@@ -40,9 +39,6 @@ for (file in files) {
     data_ppt$Prolific_ID <- dat$prolific_id
   }
   
-  
-  data_ppt$Reward <- rawdata[rawdata$screen == "demographics_debrief", "Reward"]
-  
   # Demographics
   demog <- jsonlite::fromJSON(rawdata[rawdata$screen == "demographic_questions", ]$response)
   
@@ -50,11 +46,14 @@ for (file in files) {
   demog$Education <- ifelse(demog$Education == "other", demog$`Education-Comment`, demog$Education)
   demog$`Education-Comment` <- NULL
   
-  # #Discipline
+  #Discipline
   demog$Discipline <- ifelse(!is.null(demog$Discipline), demog$Discipline, NA)
   demog$Discipline <- ifelse(demog$Discipline == "other", demog$`Discipline-Comment`, demog$Discipline)
   demog$`Discipline-Comment` <- NULL
- 
+  
+  #Student
+  demog$Student <- ifelse(!is.null(demog$Student), demog$Student, NA)
+  
   #ethnicity
   demog$Ethnicity <- ifelse(!is.null(demog$Ethnicity), demog$Ethnicity, NA)
   demog$Ethnicity <- ifelse(demog$Ethnicity == "other", demog$`Ethnicity-Comment`, demog$Ethnicity)
@@ -76,50 +75,41 @@ for (file in files) {
   data_ppt$pcs_armimmobile <- as.integer(unlist(jsonlite::fromJSON(rawdata[rawdata$screen == "pcs_armrimmobile_r", "response"])))
   data_ppt$pcs_music <- as.integer(unlist(jsonlite::fromJSON(rawdata[rawdata$screen == "pcs_music_r", "response"])))
   
-  # use this line for now as there were two screens with the same name
-  data_ppt$pcs_negativehallucination <- jsonlite::fromJSON(rawdata$response[rawdata$screen == "pcs_balls_mc"][2])$NegativeVisualHallucination_r
   # data_ppt$pcs_negativehallucination <- jsonlite::fromJSON(rawdata[rawdata$screen == "pcs_negativehallucination_r", "response"])
+  data_ppt$pcs_negativehallucination <- as.integer(unlist(jsonlite::fromJSON(rawdata[rawdata$screen == "pcs_negativehallucination", "response"])))
   
-  # PostSessionExp <- jsonlite::fromJSON(rawdata$response[rawdata$screen == "pcs_pss_r"])
+  PostSessionExp <- jsonlite::fromJSON(rawdata$response[rawdata$screen == "pcs_pss_r"])
   # data_ppt$urgepress <- taste$PostSessionExperiencea_ur
-  # data_ppt$memorypress <- taste$PostSessionExperienceb_mr
+  data_ppt$urgepress <- taste$PostSessionExperiencea_r
+  data_ppt$memorypress <- taste$PostSessionExperienceb_mr
   
   data_ppt$pcs_rememeber <- as.integer(unlist(jsonlite::fromJSON(rawdata[rawdata$screen == "pcs_remember_r", "response"])))
+
+  # Initialise all values as FALSE
+  data_ppt$pcs_yellowball <- FALSE
+  data_ppt$pcs_redball <- FALSE
+  data_ppt$pcs_greenball <- FALSE
+  data_ppt$pcs_blueball <- FALSE
+  data_ppt$pcs_purpleball <- FALSE
+  data_ppt$pcs_orangeball <- FALSE
+  data_ppt$pcs_noballs <- FALSE
   
-  # # Create columns for each color with FALSE (balls visual illusion)
-  # data_ppt$balls_red <- FALSE
-  # data_ppt$balls_green <- FALSE
-  # data_ppt$balls_blue <- FALSE
-  # data_ppt$balls_yellow <- FALSE
-  # data_ppt$balls_purple <- FALSE
-  # data_ppt$balls_orange <- FALSE
-  # data_ppt$balls_noballs <- FALSE
-  # 
-  # # Loop through each row where screen == "pcs_balls_mc"
-  # for (i in which(rawdata$screen == "pcs_balls_mc")) {
-  #   response <- fromJSON(rawdata$response[i])
-  #   
-  #   # Initialize all values for this row as FALSE
-  #   ball_values <- c("Red" = FALSE, "Green" = FALSE, "Blue" = FALSE, "Yellow" = FALSE, 
-  #                    "Purple" = FALSE, "Orange" = FALSE, "No Balls Were Presented" = FALSE)
-  #   
-  #   # Loop over the balls selected in the response and update the respective column
-  #   for (val in response$Balls_mc) {
-  #     if (val %in% names(ball_values)) {
-  #       ball_values[val] <- TRUE
-  #     }
-  #   }
+  # Only proceed if there's a non-empty response for pcs_balls_mc
+  if (any(rawdata$screen == "pcs_balls_mc")) {
+    response_raw <- rawdata$response[rawdata$screen == "pcs_balls_mc"]
     
-  #   # Now assign the updated ball values back to the respective columns in data_ppt for this row
-  #   data_ppt$balls_red[i] <- ball_values["Red"]
-  #   data_ppt$balls_green[i] <- ball_values["Green"]
-  #   data_ppt$balls_blue[i] <- ball_values["Blue"]
-  #   data_ppt$balls_yellow[i] <- ball_values["Yellow"]
-  #   data_ppt$balls_purple[i] <- ball_values["Purple"]
-  #   data_ppt$balls_orange[i] <- ball_values["Orange"]
-  #   data_ppt$balls_noballs[i] <- ball_values["No Balls Were Presented"]
-  # }
-  
+    # Extract the response (in case it's a list or multiple entries, take the first)
+    response <- jsonlite::fromJSON(response_raw[1])$Balls_mc
+    
+    # Set TRUE where appropriate
+    data_ppt$pcs_yellowball <- "Yellow" %in% response
+    data_ppt$pcs_redball    <- "Red" %in% response
+    data_ppt$pcs_greenball  <- "Green" %in% response
+    data_ppt$pcs_blueball   <- "Blue" %in% response
+    data_ppt$pcs_purpleball <- "Purple" %in% response
+    data_ppt$pcs_orangeball <- "Orange" %in% response
+    data_ppt$pcs_noballs <- "No Balls Were Presented" %in% response
+  }
 
   # PCS write up
   data_ppt$pcs_amnesia_w <- jsonlite::fromJSON(rawdata[rawdata$screen =="pcs_amnesia_w", "response"])
@@ -133,6 +123,7 @@ for (file in files) {
   # PCS manipulation 
   hello <- jsonlite::fromJSON(rawdata[rawdata$screen == "pcs_audiotest", ]$response)
   data_ppt$pcs_hello <- hello$AudioTest
+  data_ppt$pcs_press <- rawdata[rawdata$screen == "pcs_press", "keyPressCount"]
   
   # Questionnaires =====================================================================
   
@@ -165,30 +156,30 @@ for (file in files) {
   
   
   ipip6 <- jsonlite::fromJSON(rawdata[rawdata$screen == "questionnaire_ipip6", "response"])
-  data_ppt$ipip_cons3 <- ipip6$Conscientiousness_3
-  data_ppt$ipip_cons10 <- ipip6$Conscientiousness_10
-  data_ppt$ipip_cons11_R <- ipip6$Conscientiousness_11_R
-  data_ppt$ipip_cons22_R <- ipip6$Conscientiousness_22_R
-  data_ppt$ipip_ext1 <- ipip6$Extraversion_1
-  data_ppt$ipip_ext7_R <- ipip6$Extraversion_7_R
-  data_ppt$ipip_ext19_R <- ipip6$Extraversion_19_R
-  data_ppt$ipip_ext23 <- ipip6$Extraversion_23
-  data_ppt$ipip_ext2 <- ipip6$Agreeableness_2
-  data_ppt$ipip_agr8_R <- ipip6$Agreeableness_8_R
-  data_ppt$ipip_agr14 <- ipip6$Agreeableness_14
-  data_ppt$ipip_agr20_R <- ipip6$Agreeableness_20_R
-  data_ppt$ipip_honhum6_R <- ipip6$HonestyHumility_6_R
-  data_ppt$ipip_honhum12_R <- ipip6$HonestyHumility_12_R
-  data_ppt$ipip_honhum18_R <- ipip6$HonestyHumility_18_R
-  data_ppt$ipip_honhum24_R <- ipip6$HonestyHumility_24_R
-  data_ppt$ipip_neuro4 <- ipip6$Neuroticism_4
-  data_ppt$ipip_neuro15_R <- ipip6$Neuroticism_15_R
-  data_ppt$ipip_neuro16 <- ipip6$Neuroticism_16
-  data_ppt$ipip_neuro17_R <- ipip6$Neuroticism_17_R
-  data_ppt$ipip_open5 <- ipip6$Openness_5
-  data_ppt$ipip_open9_R <- ipip6$Openness_9_R
-  data_ppt$ipip_open13_R <- ipip6$Openness_13_R
-  data_ppt$ipip_open21_R <- ipip6$Openness_21_R
+  data_ppt$ipip_Cons3 <- ipip6$Conscientiousness_3
+  data_ppt$ipip_Cons10 <- ipip6$Conscientiousness_10
+  data_ppt$ipip_Cons11_R <- ipip6$Conscientiousness_11_R
+  data_ppt$ipip_Cons22_R <- ipip6$Conscientiousness_22_R
+  data_ppt$ipip_Ext1 <- ipip6$Extraversion_1
+  data_ppt$ipip_Ext7_R <- ipip6$Extraversion_7_R
+  data_ppt$ipip_Ext19_R <- ipip6$Extraversion_19_R
+  data_ppt$ipip_Ext23 <- ipip6$Extraversion_23
+  data_ppt$ipip_Agr2 <- ipip6$Agreeableness_2
+  data_ppt$ipip_Agr8_R <- ipip6$Agreeableness_8_R
+  data_ppt$ipip_Agr14 <- ipip6$Agreeableness_14
+  data_ppt$ipip_Agr20_R <- ipip6$Agreeableness_20_R
+  data_ppt$ipip_HonHum6_R <- ipip6$HonestyHumility_6_R
+  data_ppt$ipip_HonHum12_R <- ipip6$HonestyHumility_12_R
+  data_ppt$ipip_HonHum18_R <- ipip6$HonestyHumility_18_R
+  data_ppt$ipip_HonHum24_R <- ipip6$HonestyHumility_24_R
+  data_ppt$ipip_Neuro4 <- ipip6$Neuroticism_4
+  data_ppt$ipip_Neuro15_R <- ipip6$Neuroticism_15_R
+  data_ppt$ipip_Neuro16 <- ipip6$Neuroticism_16
+  data_ppt$ipip_Neuro17_R <- ipip6$Neuroticism_17_R
+  data_ppt$ipip_Open5 <- ipip6$Openness_5
+  data_ppt$ipip_Open9_R <- ipip6$Openness_9_R
+  data_ppt$ipip_Open13_R <- ipip6$Openness_13_R
+  data_ppt$ipip_Open21_R <- ipip6$Openness_21_R
   
   alldata_sub <- rbind(data_ppt, alldata_sub)
   
@@ -205,8 +196,8 @@ for (file in files) {
   df_ig$ISI <- ig$isi
   df_ig$RT <- as.numeric(ig$rt)/ 1000  # In seconds
   df_ig$Response <- ig$response
-  df_ig$Response_Correct <- ig$correct_response
-  df_ig$Error <- as.integer(ig$correct == FALSE)
+  df_ig$Response_Correct <- ig$correct
+  df_ig$Error <- as.integer(ig$correct == "false")
   
   # Reorder Columns
   first_column <- df_ig$participant
